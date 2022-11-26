54345c99f6d722fd5f92e329ee02c297a3aaf971
MarieChanaron <chanaronmarie@gmail.com>
2022-11-26 15:47:08 +0100

    geo location app properly working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { LongitudeLatitudeDisplay } from './components/LongitudeLatitudeDisplay.js';
+import { HemisphereDisplay } from './components/HemisphereDisplay.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>Geo Locator</h1>
+      <LongitudeLatitudeDisplay />
+      <HemisphereDisplay />
+    </main>
+  )
 }
 
 export default App;
